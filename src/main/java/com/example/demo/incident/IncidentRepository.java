82966005c9c293ca01bd70bb8387871e101a07d1
Ganesan J <ganesan.j@prodapt.com>
2023-10-16 22:30:27 +0530

    fixed bugs in incident repo

--- src/main/java/com/example/demo/incident/IncidentRepository.java
+++ src/main/java/com/example/demo/incident/IncidentRepository.java
@@ -9,13 +9,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IncidentRepository extends JpaRepository<Incident,Integer>{
-	@Query(value="select * from incident where assignmentGroup_Id=?1",nativeQuery=true)
+	@Query(value="select * from incident where assignement_group_id=?1",nativeQuery=true)
 	public List<Incident> findIncidentsbyGroup(int id);
 	
-	@Query(value="select * from incident where assignedTo_Id=?1",nativeQuery=true)
+	@Query(value="select * from incident where assigned_to_td=?1",nativeQuery=true)
 	public List<Incident> findIncidentsbyMember(int id);
 	
-	@Query(value="select * from incident where issue_Id=?1",nativeQuery=true)
+	@Query(value="select * from incident where issue_id=?1",nativeQuery=true)
 	public List<Incident> findIncidentsbyIssue(int id);
 	
 	@Query(value="select * from incident where ?1",nativeQuery=true)
