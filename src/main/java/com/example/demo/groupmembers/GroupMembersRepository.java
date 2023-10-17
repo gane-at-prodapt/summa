deabb27181606faf8c852d5692045df3d4f2045e
pri <priyankaramesh101@gmail.com>
2023-10-17 09:42:50 +0530

    fixed bugs in incident and groupmembers

--- src/main/java/com/example/demo/groupmembers/GroupMembersRepository.java
+++ src/main/java/com/example/demo/groupmembers/GroupMembersRepository.java
@@ -9,13 +9,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GroupMembersRepository extends JpaRepository<GroupMembers, Integer>{
-	@Query(value="select * from groupmembers where memberId=?1",nativeQuery=true)
+	@Query(value="select * from group_members where member_id=?1",nativeQuery=true)
 	public List<GroupMembers> findGroupsByMember(int id);
 	
-	@Query(value="select * from groupmembers where groupId=?1",nativeQuery=true)
+	@Query(value="select * from group_members where group_id=?1",nativeQuery=true)
 	public List<GroupMembers> findMembersByGroup(int id);
 	
-	@Query(value="select * from groupmembers where groupId=?1 and memberId=?2",nativeQuery=true)
+	@Query(value="select * from group_members where group_id=?1 and member_id=?2",nativeQuery=true)
 	public GroupMembers find(int groupId,int memberId);
 	
 }
